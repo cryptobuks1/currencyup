d49bae8387f9e96c611df4e88252aaf8b32b8f56
Joel Cañas <joeladolfo2017@gmail.com>
2020-04-04 22:33:38 -0400

    Changes in Exchange

--- server/modules/private_constants.js
+++ server/modules/private_constants.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.recaptcha_priv_key = '6LesbeMUAAAAAMEoTvkORWgGWEnJqvGXxfdgc_uR';
+exports.recaptcha_priv_key = '6LfJxeYUAAAAACTnWMxNSavpL4X8ZIylE4aJbvg7';
 exports.password_private_suffix = '82247203';
 exports.SSL_KEY = '/etc/letsencrypt/live/dev.currencyup.com/privkey.pem'; //change to your ssl certificates private key
 exports.SSL_CERT = '/etc/letsencrypt/live/dev.currencyup.com/fullchain.pem'; //change to your ssl certificates fullchain
